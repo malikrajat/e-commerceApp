44228f004fedc32a05dde16dc1d3a184984ea1ed
Array <arrayjson@gmail.com>
2023-09-23 20:34:40 +0530

    create login form and add validation

--- libs/user/src/lib/login/login.component.ts
+++ libs/user/src/lib/login/login.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { AsyncPipe, JsonPipe, NgIf } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @Component({
-  selector: 'org-login',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+	selector: 'org-login',
+	standalone: true,
+	imports: [ReactiveFormsModule, MatInputModule, NgIf, MatButtonModule, JsonPipe, AsyncPipe, MatFormFieldModule],
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+	search: FormControl<string> = new FormControl();
+	loginForm = this.fb.group({
+		username: new FormControl<string>(
+			'',
+			{
+				nonNullable: true,
+				validators: [
+					Validators.required,
+					Validators.email
+				]
+			}
+		),
+		password: new FormControl<string>(
+			'',
+			{
+				nonNullable: true,
+				validators: [
+					Validators.required,
+					Validators.minLength(6),
+					Validators.maxLength(8)
+				]
+			}
+		)
+	})
+
+	constructor(
+		private readonly fb: FormBuilder
+	) {
+	}
+
+	login() {
+
+	}
 }
