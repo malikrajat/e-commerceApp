78eb6a53b1838174d90da619ac72d1d76a41096c
Array <arrayjson@gmail.com>
2023-09-23 08:38:17 +0530

    create routing for login

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Route } from '@angular/router';
 import { provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import {
-  loadProducts,
-  loadProductsByCategory,
-  productFeature,
-} from '@org/product';
+import { loadProducts, loadProductsByCategory, productFeature, } from '@org/product';
 import { cartFeature, loadCart } from '@org/cart';
 
 export const appRoutes: Route[] = [
@@ -19,7 +15,7 @@ export const appRoutes: Route[] = [
     loadComponent: () => import('@org/product').then((m) => m.ProductComponent),
     providers: [
       provideState(productFeature),
-      provideEffects({ loadProducts, loadProductsByCategory }),
+      provideEffects({loadProducts, loadProductsByCategory}),
     ],
   },
   {
@@ -30,12 +26,17 @@ export const appRoutes: Route[] = [
     },
     providers: [
       provideState(productFeature),
-      provideEffects({ loadProducts, loadProductsByCategory }),
+      provideEffects({loadProducts, loadProductsByCategory}),
     ],
   },
   {
     path: 'cart',
     loadComponent: () => import('@org/cart').then((m) => m.CartComponent),
-    providers: [provideState(cartFeature), provideEffects({ loadCart })],
+    providers: [provideState(cartFeature), provideEffects({loadCart})],
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('@org/user').then((m) => m.LoginComponent),
+    providers: [provideState(cartFeature), provideEffects({loadCart})],
   },
 ];
---
+++ libs/user/src/lib/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'org-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+}
