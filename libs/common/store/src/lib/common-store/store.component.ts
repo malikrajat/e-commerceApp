a3611d422c1905d26408774eb970001b2ad87364
malikrajat <mr.rajatmalik@gmail.com>
2023-10-01 08:58:34 +0530

    generate common store
    Signed-off-by: malikrajat <mr.rajatmalik@gmail.com>

---
+++ libs/common/store/src/lib/common-store/store.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+	selector: 'org-store',
+	standalone: true,
+	imports: [CommonModule],
+	templateUrl: './store.component.html',
+	styleUrls: ['./store.component.css'],
+})
+export class StoreComponent {
+}
